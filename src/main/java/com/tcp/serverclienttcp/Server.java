ca177c3915a5e9ea6bf139a800be7fac92eeecc4
SebastianIordan05 <sebastian.iordan@edu.iisleviponti.it>
2024-05-05 21:06:46 +0200

    first commit

---
+++ src/main/java/com/tcp/serverclienttcp/Server.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tcp.serverclienttcp;
+
+/**
+ *
+ * @author seba2
+ */
+public class Server {
+    public static void main(String[] args) {
+        
+    }
+}
